7a61122696e879e247364ee43123fb53e8d35192
BoazAB <32714@ma-web.nl>
2021-11-10 12:04:34 +0100

    verbetering countdown

--- PYTB1L4TheFinalCountdown/PYTB1L4TheFinalCountdown.py
+++ PYTB1L4TheFinalCountdown/PYTB1L4TheFinalCountdown.py
@@ -1,7 +1,6 @@
 import time
-i=100
+i=1000
 while i > 0:
     i -= 1
     print(i)
-    time.sleep(1)
-print("https://www.youtube.com/watch?v=2oYhubLUUyE")
+print("https://www.youtube.com/watch?v=M5V_IXMewl4")
